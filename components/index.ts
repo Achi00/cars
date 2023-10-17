3ffd674270dbb32a8da5c4e2e31250475bdb57ef
unknown <toolvlg@gmail.com>
2023-10-17 16:20:55 +0400

    deploy 3

--- components/index.ts
+++ components/index.ts
@@ -1,19 +1,15 @@
-import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Content from "./Content";
 import CustomFilter from "./CustomFilter";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
-import Scene from "./Scene";
 
 export {
-  Hero,
   CustomButton,
   Content,
   Navbar,
   CustomFilter,
   SearchManufacturer,
   SearchBar,
-  Scene,
 };
