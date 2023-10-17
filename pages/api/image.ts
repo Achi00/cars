ec60da85d595e452b69da1d9aa6587f761af8f65
unknown <toolvlg@gmail.com>
2023-10-17 16:30:06 +0400

    deploy 3

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -1,31 +1,31 @@
-import { Configuration, OpenAIApi } from "openai";
+// import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const openai = new OpenAIApi(configuration);
+// const openai = new OpenAIApi(configuration);
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+// export default async function handler(req, res) {
+//   if (req.method !== "POST") {
+//     return res.status(405).json({ message: "Method not allowed" });
+//   }
 
-  try {
-    const { prompt } = req.body;
+//   try {
+//     const { prompt } = req.body;
 
-    if (!prompt) {
-      return res.status(400).json({ error: "Prompt is required" });
-    }
+//     if (!prompt) {
+//       return res.status(400).json({ error: "Prompt is required" });
+//     }
 
-    const response = await openai.createImage({
-      prompt: prompt,
-      n: 1,
-      size: "512x512",
-    });
+//     const response = await openai.createImage({
+//       prompt: prompt,
+//       n: 1,
+//       size: "512x512",
+//     });
 
-    return res.status(200).json({ data: response.data.data });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-}
+//     return res.status(200).json({ data: response.data.data });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// }
